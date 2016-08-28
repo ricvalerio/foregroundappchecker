9e5c5c19049700adf44fcde19237c78a5184e65a
ricardo <ricardo.valerio@gmail.com>
2016-08-28 22:23:38 +0200

    changed LollipopDetector to search for latest MOVE_TO_FOREGROUND event to get foreground app

--- fgchecker/src/main/java/com/rvalerio/fgchecker/detectors/LollipopDetector.java
+++ fgchecker/src/main/java/com/rvalerio/fgchecker/detectors/LollipopDetector.java
@@ -2,21 +2,16 @@ package com.rvalerio.fgchecker.detectors;
 
 import android.annotation.TargetApi;
 import android.app.Service;
-import android.app.usage.UsageStats;
+import android.app.usage.UsageEvents;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.os.Build;
 
 import com.rvalerio.fgchecker.Utils;
 
-import java.util.List;
-import java.util.SortedMap;
-import java.util.TreeMap;
-
 public class LollipopDetector implements Detector {
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public String getForegroundApp(Context context) {
+    public String getForegroundApp(final Context context) {
         if(!Utils.hasUsageStatsPermission(context))
             return null;
 
@@ -24,17 +19,16 @@ public class LollipopDetector implements Detector {
 
         UsageStatsManager mUsageStatsManager = (UsageStatsManager) context.getSystemService(Service.USAGE_STATS_SERVICE);
         long time = System.currentTimeMillis();
-        List<UsageStats> stats = mUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 60, time);
-        if (stats != null) {
-            SortedMap<Long, UsageStats> mySortedMap = new TreeMap<Long, UsageStats>();
-            for (UsageStats usageStats : stats) {
-                mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
-            }
-            if (mySortedMap != null && !mySortedMap.isEmpty()) {
-                foregroundApp = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+
+        UsageEvents usageEvents = mUsageStatsManager.queryEvents(time - 1000 * 3600, time);
+        UsageEvents.Event event = new UsageEvents.Event();
+        while (usageEvents.hasNextEvent()) {
+            usageEvents.getNextEvent(event);
+            if(event.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+                foregroundApp = event.getPackageName();
             }
         }
 
-        return foregroundApp;
+        return foregroundApp ;
     }
 }
